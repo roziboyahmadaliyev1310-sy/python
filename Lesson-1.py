dd00618aef1310f85671643e3d19eab320912f93
A_Roziboy <roziboyahmadaliyev1310@gmail.com>
2026-03-07 14:08:14 +0900

    repleting python deeply

--- Lesson-1.py
+++ Lesson-1.py
@@ -1,7 +1,22 @@
-# input user 
-age = input("What is your age? ")
-name = input("What is your name? ")
+# input ==> Function that allows to take users input and the data type 
+            # of the input is string.
+            
+# age = input("What is your age? ")
+# name = input("What is your name? ")
 
-print("Your age is: " + age)
-print("Your name is: " + name)
+# print("Your age is: " + age)
+# print("Your name is: " + name)
 
+# Finding the date of birth
+
+print("This programm will find your birth year.")
+print("-"*30)
+age=int(input("What is your age? "))
+
+print(f"Your birth year is {2026 - age}")
+
+product=input("What you want to buy? ")
+price=float(input("What is the price of" + product +"?"))
+quantity=int(input("How many want to buy? "))
+total=price*quantity
+print(f"The total price for {quantity} {product}(s) is: ${total}")
---
+++ Lesson-2.py
@@ -0,0 +1,55 @@
+# Agment operation
+# +=, -=,*=,/=,//=,**=
+
+# age=25
+# age+=1
+# print("Next year you will be ", age)
+
+# Round function
+
+pi=3.14159
+print(round(pi,2))
+
+price=19.99
+print(round(price))
+
+# abs() function => returns the absolute value of a number
+
+number=-4
+print(abs(number))
+
+# max() and min() functions
+
+array=[3,4,6,7,8,2,1]
+print(max(array))
+print(min(array))
+
+# pow() function => returns the value of x to the power of y
+
+x=3
+y=3
+print(pow(x,y))
+
+# sum() function => returns the sum of all items in an iterable
+
+numbers=[1,2,3,4,5,6]
+print(sum(numbers))
+
+# len() function => returns the number of items in an object
+string='Hello World'
+print(len(string))
+print(len(numbers))
+
+# math module => provides mathematical functions
+import math
+
+x=9
+
+# sqrt() function => returns the square root of a number
+print(math.sqrt(x))
+
+# ceil() function => returns the smallest integer greater than or equal to a number
+print(math.ceil(3.2))
+
+# floor() function => returns the largest integer less than or equal to a number
+print(math.floor(3.99))
